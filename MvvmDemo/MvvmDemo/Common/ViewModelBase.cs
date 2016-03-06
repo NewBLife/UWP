898ace427279ec3d198d22c364dad2127b9e7681
李暁賓 <newblifes@hotmail.com>
2016-03-06 23:28:12 +0900

    新建

---
+++ MvvmDemo/MvvmDemo/Common/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MvvmDemo.Common
+{
+    /// <summary>
+    /// Viewmodel基类，属性双向绑定基础
+    /// </summary>
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// 属性变更通知
+        /// </summary>
+        /// <param name="propertyName">属性名</param>
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
